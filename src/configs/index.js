c7879c55c60f0746806878977a2076febb1da68f
realdao001 <realdao@protonmail.com>
2021-01-06 17:05:21 +0800

    move query parser to config module

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,13 @@
+import { parseQuery } from '../lib/parser.js'
+
 export async function getConfig(env) {
+  if (!env) {
+    const query = parseQuery(window.location.search)
+    env = query.env || 'test'
+  }
   const generalConfig = {
     refreshInterval: 10000,
+    env,
   }
   const envConfig = await import(`./config.${env}.js`)
   return Object.assign(generalConfig, envConfig.default)
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,9 @@
 import { EventEmitter } from '../lib/event-emitter.js'
-import { parseQuery } from '../lib/parser.js'
 import { loadServices, loadModules } from '../lib/loader.js'
 import { getConfig } from '../configs/index.js'
 
 async function main() {
-  const query = parseQuery(window.location.search)
-  const env = query.env || 'test'
-  const config = await getConfig(env)
-  config.env = env
+  const config = await getConfig()
   console.log('config:', config)
 
   const service = await loadServices(['../services/realdao.js', '../services/wallet.js'], { config })
